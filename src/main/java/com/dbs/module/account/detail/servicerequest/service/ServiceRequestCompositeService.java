70131cb52ee897cb94b9e4473f3c87f60627e666
Vitrio Wibisono <vitrio@noxus.co.id>
2025-11-22 06:11:34 +0700

    Fix service request compilation errors after dev merge
    - Fix FlowStatus import: changed from com.dbs.common.base.utils to com.dbs.common.library.utils
    - Fix ServiceRequestService: changed getListGlobalTypeValue to getGlobalTypeOtherWithValue
    - Fix ServiceRequestCompositeService: changed setFileCategory to setCategory
    - Fix ServiceRequestCompositeService: inject RGlobalTypeValueRepo and use findById instead of getGlobalTypeValueById
    
    These fixes ensure compatibility with the merged dev branch changes.

--- src/main/java/com/dbs/module/account/detail/servicerequest/service/ServiceRequestCompositeService.java
+++ src/main/java/com/dbs/module/account/detail/servicerequest/service/ServiceRequestCompositeService.java
@@ -1,7 +1,7 @@
 package com.dbs.module.account.detail.servicerequest.service;
 
 import com.dbs.common.base.utils.Constant;
-import com.dbs.common.base.utils.FlowStatus;
+import com.dbs.common.library.utils.FlowStatus;
 import com.dbs.common.base.utils.ResponseUtils;
 import com.dbs.common.library.ctrl.ResponseObject;
 import com.dbs.common.library.services.GlobalTypeValueService;
@@ -16,6 +16,7 @@ import com.dbs.database.crm.repositories.accountmanagement.Account.MContactRepo;
 import com.dbs.database.crm.repositories.accountmanagement.servicerequest.NxMServiceRequestRepo;
 import com.dbs.database.crm.repositories.accountmanagement.servicerequest.NxRSrDetailsRepo;
 import com.dbs.database.crm.repositories.usermanagement.MAttachmentRepo;
+import com.dbs.database.crm.repositories.usermanagement.RGlobalTypeValueRepo;
 import com.dbs.module.account.detail.servicerequest.dto.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -69,6 +70,9 @@ public class ServiceRequestCompositeService {
 
     @Autowired
     private GlobalTypeValueService globalTypeService;
+
+    @Autowired
+    private RGlobalTypeValueRepo rGlobalTypeValueRepo;
 
     @Autowired
     private Validator validator;
@@ -419,7 +423,7 @@ public class ServiceRequestCompositeService {
             // Create attachment record
             M_ATTACHMENT attachment = new M_ATTACHMENT();
             attachment.setFileName(attachDto.getFileName());
-            attachment.setFileCategory(attachDto.getCategory());
+            attachment.setCategory(attachDto.getCategory());
             attachment.setType(attachDto.getType());
             attachment.setDescription(attachDto.getDescription());
             attachment.setFileSize(attachDto.getFileSize());
@@ -890,8 +894,8 @@ public class ServiceRequestCompositeService {
      */
     private String getStatusName(Integer statusId) {
         try {
-            var status = globalTypeService.getGlobalTypeValueById(statusId);
-            return status != null ? status.getGlbValue() : "UNKNOWN";
+            var status = rGlobalTypeValueRepo.findById(statusId);
+            return status.isPresent() ? status.get().getGlbValue() : "UNKNOWN";
         } catch (Exception e) {
             logger.warn("Could not retrieve status name from GLOBAL_TYPE", e);
             return "UNKNOWN";
--- src/main/java/com/dbs/module/account/detail/servicerequest/service/ServiceRequestService.java
+++ src/main/java/com/dbs/module/account/detail/servicerequest/service/ServiceRequestService.java
@@ -1,7 +1,7 @@
 package com.dbs.module.account.detail.servicerequest.service;
 
 import com.dbs.common.base.utils.Constant;
-import com.dbs.common.base.utils.FlowStatus;
+import com.dbs.common.library.utils.FlowStatus;
 import com.dbs.common.base.utils.MaterialTablePagingRequest;
 import com.dbs.common.base.utils.PagingUtils;
 import com.dbs.common.base.utils.ResponseUtils;
@@ -303,7 +303,7 @@ public class ServiceRequestService {
     public ResponseEntity<ResponseObject> getRequestTypes() {
         ResponseObject result;
         try {
-            var types = globalTypeService.getListGlobalTypeValue("Service Request Type");
+            var types = globalTypeService.getGlobalTypeOtherWithValue("Service Request Type");
             result = new ResponseObject(ResponseUtils.SUCCESS_TRUE, HttpStatus.OK,
                 "Success", types);
             return new ResponseEntity<>(result, result.getHttpCode());
